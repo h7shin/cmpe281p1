7e55bb8dcd43dd7f342c727a7a572cfba4b0cd2f
Hyunwook Shin <shincontact@icloud.com>
2017-10-21 21:05:14 +0000

    Handle GET request from server

--- src/backend/dumbserver.py
+++ src/backend/dumbserver.py
@@ -1,13 +1,16 @@
-#!/usr/bin/env/python
+#!/usr/bin/env python
+
 
 '''
 This is a barebone server implementation
 To be used by the bacckend EC2 servers
 '''
 
+import BaseHTTPServer
+
 WEBSERVER_PORT = 8000
 
-class HTTPHandler(object):
+class HTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
    '''
    handle requests based on the method
    '''
@@ -15,15 +18,15 @@ class HTTPHandler(object):
       ''' handles post request '''
       pass
 
-   def get(self):
+   def do_GET(self): #pylint: disable=invalid-name
       ''' handles get request '''
-      pass
+      self.send_response(200)
 
 def run_backend_server():
    '''
    run HTTP server listening to the port
    '''
-   pass
+   BaseHTTPServer.HTTPServer(('', WEBSERVER_PORT), HTTPHandler).serve_forever()
 
 if __name__ == '__main__':
    run_backend_server()
