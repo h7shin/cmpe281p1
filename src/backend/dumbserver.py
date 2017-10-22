625d1b9dd861790174c7ee203ed9f7be7ef271d7
Hyunwook Shin <shincontact@icloud.com>
2017-10-22 01:25:37 +0000

    Sync AWS keys and also add url parsing

--- src/backend/dumbserver.py
+++ src/backend/dumbserver.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
-
 '''
 This is a barebone server implementation
-To be used by the bacckend EC2 servers
+To be used by the backend EC2 servers
 '''
 
 import BaseHTTPServer
+import boto3
+import os
+
+AWS_ACCESS_ID = os.environ.get( 'AWS_ACCESS_ID' )
+AWS_SECRET_KEY = os.environ.get( 'AWS_SECRET_KEY' )
+assert len( AWS_ACCESS_ID ) > 0, 'Please set AWS access key'
+assert len( AWS_SECRET_KEY ) > 0, 'Please set AWS secret key'
 
 WEBSERVER_PORT = 8000
 
@@ -20,10 +26,15 @@ class HTTPHandler( BaseHTTPServer.BaseHTTPRequestHandler ):
 
    def do_GET( self ): #pylint: disable=invalid-name
       ''' handles get request '''
+      def parseQueryString( path ):
+         tokens = path.replace( '/?', '' ).split( '&' )
+         return [ t.split( '=' ) for t in tokens if t ]
+
       self.send_response( 200 )
       self.send_header( 'Content-type', 'text/html' )
       self.end_headers()
-      self.wfile.write( self.path )
+      tokenized = parseQueryString( self.path )
+      self.wfile.write( str(tokenized) )
 
 def run_backend_server():
    '''
