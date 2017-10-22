866fd08c811459ab91b8b8f2ff37045efb6275d1
Hyunwook Shin <hyunwook.james.shin@gmail.com>
2017-10-22 13:40:55 -0700

    More formatting

--- src/frontend/update.php
+++ src/frontend/update.php
@@ -7,7 +7,8 @@
       CloudFront Project 1
       </title>
 	  <script type="text/javascript" src="js/info.js"></script>
-      <script type="text/javascript" src="js/upload.js"></script>         
+      <script type="text/javascript" src="js/upload.js"></script>      
+	  <script type="text/javascript" src="js/update.js"></script> 	  
       <link rel="stylesheet" href="css/main.css">
    </head>
    <body onload="populateBannerFields( <? echo $fileId ?> ); addUploadBox( 'update',  <? echo $fileId ?>)">
@@ -18,7 +19,7 @@
          </div>
 		<? include 'fileinfo.php'; ?>
 		 <div id="response">
-		    <button class="clicker">Confirm Update</button>
+		    <button class="clicker" onClick="updateFile( <? echo $fileId ?> )">Confirm Update</button>
 			<button class="clicker" onClick="window.location.href= 'index.html'">Cancel</button>
 		 </div>
 		 <div id="status">
