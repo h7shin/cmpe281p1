9c43570813f9cebf1c1a5f58d931ffd15a2319c4
Hyunwook Shin <shincontact@icloud.com>
2017-10-22 07:51:31 +0000

    Implement POST handling, upload file to S3

--- src/backend/dumbserver.py
+++ src/backend/dumbserver.py
@@ -9,8 +9,11 @@ from rdshandle import rdshandle
 import BaseHTTPServer
 import boto3
 import os
+import md5
 import json
 import cloudfront
+import tempfile
+import cgi
 
 AWS_ACCESS_ID = os.environ.get( 'AWS_ACCESS_ID' )
 AWS_SECRET_KEY = os.environ.get( 'AWS_SECRET_KEY' )
@@ -19,13 +22,84 @@ assert len( AWS_SECRET_KEY ) > 0, 'Please set AWS secret key'
 
 WEBSERVER_PORT = 8000
 
+def writeTempFile( content ):
+   '''
+   Writes a content to a temporary file and returns the path to it
+   '''
+   n = tempfile.NamedTemporaryFile( delete=False )
+   n.write( content )
+   n.close()
+   return n.name
+
+def toS3( filecontent ):
+   path = writeTempFile( filecontent )
+   print path
+   s3 = boto3.resource('s3',
+                        aws_access_key_id=AWS_ACCESS_ID,
+                        aws_secret_access_key=AWS_SECRET_KEY )
+   try:
+      binary = open( path, 'rb' )
+      # Upload  the file to S3
+      # Update RDS
+      keygen = md5.new()
+      keygen.update( filecontent )
+      hashstring = keygen.hexdigest()
+      print hashstring
+      s3.Bucket( 'shinhw2b1' ).put_object( Key=hashstring, Body=binary )
+   finally:
+      os.remove( path )
+   return hashstring
+
 class HTTPHandler( BaseHTTPServer.BaseHTTPRequestHandler ):
    '''
    handle requests based on the method
    '''
-   def post( self ):
-      ''' handles post request '''
-      pass
+   def do_POST( self ):
+      ''' handles PUT request '''
+      length = self.headers[ 'Content-Length' ]
+      username = self.headers[ 'username' ]
+      self.send_response( 200 )
+      self.send_header( 'Content-type', 'text/html' )
+      content = self.rfile.read( int( length ) )
+      lines = content.split( '\n')
+
+      # show some details to output
+      print length
+      print username
+
+      # Some sanity check on file contente
+      assert '----' in lines[0]
+      assert 'Content-Disposition' in lines[1]
+      assert 'name' in lines[1]
+      assert 'filename' in lines[1]
+      assert 'Content-Type' in lines[2]
+      assert '----' in lines [-2]
+
+      # extract the inner data
+      filename = lines[1].split( '"' )[1]
+      filecontent = '\n'.join( lines[4:-2] )
+
+      # insert into S3 bucket
+      bucketkey = toS3( filecontent )
+
+      # save into db
+      rds = rdshandle.RDS()
+      newFile = rdshandle.Object( '', filename, username, '',  bucketkey=bucketkey )
+      print newFile.bucketkey(), 'is the new bucket key'
+      rds.insert( ( newFile, ) )
+
+      result = 'success'
+      wrapper = { 'error' : '', 'result':  result }
+      self.wfile.write( json.dumps( wrapper ) )
+
+   def do_OPTIONS( self ):
+      self.send_response( 200 )
+      self.send_header( 'Access-Control-Allow-Origin', '*' )
+      self.send_header( 'Access-Control-Allow-Methods', 'OPTIONS, POST, GET' )
+      self.send_header( 'Access-Control-Allow-Headers', 'Content-type' )
+      self.send_header( 'Access-Control-Allow-Headers', 'Content-length' )
+      self.send_header( 'Access-Control-Allow-Headers', 'username' )
+      self.end_headers()
 
    def do_GET( self ): #pylint: disable=invalid-name
       ''' handles get request '''
@@ -34,6 +108,10 @@ class HTTPHandler( BaseHTTPServer.BaseHTTPRequestHandler ):
          return dict( [ t.split( '=' ) for t in tokens if '=' in t ] )
 
       self.send_response( 200 )
+      self.send_header( 'Access-Control-Allow-Origin', '*' )
+      self.send_header( 'Access-Control-Allow-Methods', 'OPTIONS, POST, GET' )
+      self.send_header( 'Access-Control-Allow-Headers', 'Content-type' )
+      self.send_header( 'Access-Control-Allow-Headers', 'Content-length' )
       self.send_header( 'Content-type', 'text/html' )
       self.end_headers()
       tokenized = parseQueryString( self.path )
